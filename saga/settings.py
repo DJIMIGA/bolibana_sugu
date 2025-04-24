3935de37a28ee12d433e8a928e3f5b877a937502
aly <pymalien@gmail.com>
2025-04-24 08:27:25 +0200

    Configuration de sécurité SSL globale

--- saga/settings.py
+++ saga/settings.py
@@ -62,23 +62,18 @@ stripe.api_key = STRIPE_SECRET_KEY
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # Configuration de sécurité SSL
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 if os.getenv('HEROKU'):
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
     SECURE_HSTS_SECONDS = 31536000  # 1 an
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
     X_FRAME_OPTIONS = 'DENY'
-else:
-    # Configuration de sécurité pour le développement local
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
 
 # Configuration des cookies
 SESSION_COOKIE_DOMAIN = None  # Retiré pour éviter les problèmes de domaine
