9f89ff2a16752b5302ad25da20055f4d141698fa
aly <pymalien@gmail.com>
2025-05-05 06:56:13 +0200

    Correction des migrations pour battery_capacity

--- saga/product/migrations/0011_remove_battery_capacity.py
+++ saga/product/migrations/0011_remove_battery_capacity.py
@@ -3,7 +3,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('product', '0010_fix_imei_field'),
+        ('product', '0001_initial'),
     ]
 
     operations = [
