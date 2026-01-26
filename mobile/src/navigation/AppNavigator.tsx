c26ffd9cd94fc686da241375981c19b171388327
aly <pymalien@gmail.com>
2026-01-26 20:54:19 +0100

    fix: correction import Linking depuis react-native au lieu de react-navigation

--- mobile/src/navigation/AppNavigator.tsx
+++ mobile/src/navigation/AppNavigator.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { View, Text, StyleSheet, Platform, Modal, TouchableOpacity } from 'react-native';
-import { NavigationContainer, useNavigationContainerRef, Linking } from '@react-navigation/native';
+import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
