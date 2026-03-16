5da3c059ae795a0f5417b95027570dacf17e3129
djimiga <pymalien@gmail.com>
2026-03-16 14:00:46 +0000

    mise à jour depuis le VPS

--- saga/settings.py
+++ saga/settings.py
@@ -588,6 +588,7 @@ if DEBUG:
     CORS_ALLOWED_ORIGINS = [
         'http://localhost:8000',
         'http://localhost:3000',
+        'http://localhost:3001',
         'http://127.0.0.1:8000',
         'http://bolibana.com',
         'http://www.bolibana.com',
